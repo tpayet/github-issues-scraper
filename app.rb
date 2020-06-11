aaea8cbc3818da636914fb39428584b9ddced6da
Thomas Payet <thomas@meilisearch.com>
2020-06-11 11:05:26 +0200

    Update fields sent to MeiliSearch

--- app.rb
+++ app.rb
@@ -8,10 +8,19 @@ logger = Logger.new(STDOUT)
 
 MeiliDocument = Struct.new(
   :content,
-  :lvl2,
-  :lvl1,
-  :lvl0,
+  :hierarchy_lvl2,
+  :hierarchy_lvl1,
+  :hierarchy_lvl0,
+  :page_rank,
   :objectID,
+  :hierarchy_radio_lvl0,
+  :hierarchy_radio_lvl1,
+  :hierarchy_radio_lvl2,
+  :hierarchy_radio_lvl3,
+  :hierarchy_radio_lvl4,
+  :hierarchy_radio_lvl5,
+  :url,
+  :anchor,
   keyword_init: true
 )
 
@@ -26,23 +35,28 @@ meili_client = MeiliSearch::Client.new(ENV['MEILISEARCH_URL'], ENV['MEILISEARCH_
 logger.info "Getting issues of #{ENV['GH_REPOSITORY']}"
 issues = gh_client.issues(ENV['GH_REPOSITORY'])
 
+
 logger.info 'Transform GitHub issues to documents for MeiliSearch'
 issues.each do |issue|
   documents_to_index << MeiliDocument.new(
-    lvl0: 'GitHub',
-    lvl1: 'Issue',
-    lvl2: issue.title,
+    hierarchy_lvl0: '🌍 GitHub',
+    hierarchy_lvl1: 'Issue',
+    hierarchy_lvl2: issue.title,
     content: issue.body,
+    page_rank: 0,
+    url: issue.html_url,
     objectID: issue.body.hash
   )
 
   issue_comments = gh_client.issue_comments(ENV['GH_REPOSITORY'], issue.number)
   issue_comments.each do |comment|
     documents_to_index << MeiliDocument.new(
-      lvl0: 'GitHub',
-      lvl1: 'Issue',
-      lvl2: issue.title,
+      hierarchy_lvl0: '🌍 GitHub',
+      hierarchy_lvl1: 'Issue',
+      hierarchy_lvl2: issue.title,
       content: comment.body,
+      page_rank: 0,
+      url: comment.html_url,
       objectID: comment.body.hash
     )
   end
@@ -54,16 +68,18 @@ pull_requests = gh_client.pull_requests(ENV['GH_REPOSITORY'])
 logger.info 'Transform GitHub pull requests to documents for MeiliSearch'
 pull_requests.each do |pr|
   documents_to_index << MeiliDocument.new(
-    lvl0: 'GitHub',
-    lvl1: 'Pull request',
-    lvl2: pr.title,
+    hierarchy_lvl0: '🌍 GitHub',
+    hierarchy_lvl1: 'Pull request',
+    hierarchy_lvl2: pr.title,
     content: pr.body,
+    page_rank: 0,
+    url: pr.html_url,
     objectID: pr.body.hash
   )
 end
 
 logger.info 'Exporting documents to MeiliSearch'
-meili_index = meili_client.create_index(uid: 'docs')
+meili_index = meili_client.index(uid: 'docs')
 meili_index.add_documents(documents_to_index.map(&:to_h))
 
 logger.info 'kthxbye'
